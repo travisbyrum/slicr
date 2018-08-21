488ebee155f28538a88c3d6a5baf6d792d5ed461
Travis Byrum <travis.tbyrum@gmail.com>
2018-08-21 16:21:48 -0400

    Create extra test fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,7 +11,8 @@ import pytest
 
 from slicr import create_app
 from slicr.config import Config
-from slicr.models import db as test_db
+from slicr.models import Domain, Link
+from slicr.extensions import db as test_db
 
 
 class CustomTestConfig(Config):
@@ -19,7 +20,7 @@ class CustomTestConfig(Config):
 
     JSONIFY_PRETTYPRINT_REGULAR = True
     SQLALCHEMY_DATABASE_URI = os.getenv(
-        'SQLALCHEMY_DATABASE_URI',
+        'TEST_SQLALCHEMY_DATABASE_URI',
         'sqlite://'
     )
     WTF_CRSF_ENABLED = False
@@ -48,6 +49,39 @@ def test_client(test_app):
 
     with test_app.test_client() as client:
         yield client
+
+
+@pytest.fixture
+def test_domain(db):
+    """Test domain fixture."""
+
+    test_domain = Domain(
+        name='test',
+        domain='test@test.gov',
+        tag='test'
+    ).save()
+
+    db.session.add(test_domain)
+    db.session.commit()
+
+    return test_domain
+
+
+@pytest.fixture
+def test_link(db):
+    """Test link fixture."""
+
+    test_url = 'https://www.google.com'
+
+    test_link = Link(
+        url=test_url,
+        salt=0
+    )
+
+    db.session.add(test_link)
+    db.session.commit()
+
+    return test_link
 
 
 # pylint: disable=C0103
